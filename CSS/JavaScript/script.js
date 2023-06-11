a672d774ed81f13adf3a88b322d85721b1cd7296
Saad Hroub <saad.hroub@gmail.com>
2023-06-11 17:45:23 +0300

    change the element text and hide it

---
+++ CSS/JavaScript/script.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    alert("Oh, I come from the JS File")
+}
+
+
+function light(element) {
+    element.innerText = "OFF";
+}
+
+function hide(element){
+    element.remove();
+}
+
+
+
